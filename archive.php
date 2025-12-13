b621f654de47fdcba54534d1879a74ee7331c5ff
kallyas <kallyasmedia@gmail.com>
2025-12-13 17:43:27 +0300

    Update archive.php
    Signed-off-by: kallyas <kallyasmedia@gmail.com>

---
+++ archive.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * The template for displaying archive pages
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Versalia
+ * @since 1.0.0
+ */
+
+declare(strict_types=1);
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+get_header();
+?>
+
+<main id="primary" class="site-main">
+
+	<?php if ( have_posts() ) : ?>
+
+		<header class="page-header">
+			<?php
+			the_archive_title( '<h1 class="page-title">', '</h1>' );
+			the_archive_description( '<div class="archive-description">', '</div>' );
+			?>
+		</header><!-- .page-header -->
+
+		<?php
+		/* Start the Loop */
+		while ( have_posts() ) :
+			the_post();
+
+			get_template_part( 'template-parts/content/content', get_post_type() );
+
+		endwhile;
+
+		the_posts_navigation();
+
+	else :
+
+		get_template_part( 'template-parts/content/content', 'none' );
+
+	endif;
+	?>
+
+</main><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
