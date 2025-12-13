8c83839b70e999ef5c88ed572fd74f374462c460
kallyas <kallyasmedia@gmail.com>
2025-12-13 17:36:18 +0300

    Update sidebar.php
    Signed-off-by: kallyas <kallyasmedia@gmail.com>

---
+++ sidebar.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The sidebar containing the main widget area
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Versalia
+ * @since 1.0.0
+ */
+
+declare(strict_types=1);
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+if ( ! is_active_sidebar( 'sidebar-1' ) ) {
+	return;
+}
+?>
+
+<aside id="secondary" class="widget-area">
+	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+</aside><!-- #secondary -->
